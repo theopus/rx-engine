45960ef0a57af357b5318edb1d19e81560529ec7
theopus <theopus@theopuss-MacBook-Pro.local>
2019-07-23 13:35:00 +0300

    upd

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,66 @@ extern crate gl;
 extern crate glfw;
 extern crate specs;
 
+use std::cell::RefCell;
 use std::fs::read;
+use std::mem;
+use std::ops::Add;
+use std::rc::Rc;
 use std::sync::mpsc::Receiver;
 
 use glfw::{Action, Context, Glfw, Key};
-use sdl2::audio::AudioStatus::Playing;
-use sdl2::hint::set;
-use std::mem;
 
 fn main() {
-
-    specs::DispatcherBuilder::new().build();
     let mut platform_manager: Box<PlatformManager> = GlfwPlatformManager::new(WindowConfig { width: 300, height: 300 });
-    let open_gl: gl::Gl = platform_manager.create_renderer(RendererType::OpenGL);
-
-    unsafe { open_gl.ClearColor(0.3, 0.3, 0.9, 1f32); }
+    let renderer: Box<Renderer> = platform_manager.create_renderer(RendererType::OpenGL);
 
     while !platform_manager.should_close() {
-        unsafe { open_gl.Clear(gl::COLOR_BUFFER_BIT); }
+        renderer.clear_color();
         platform_manager.process_events();
-        platform_manager.swap_buffers();
+        renderer.swap_buffer()
     }
 }
 
 
+trait Renderer {
+    fn swap_buffer(&self);
+
+    fn clear_color(&self);
+}
+
+struct OpenGLRenderer<'a> {
+    gl_api: Rc<gl::Gl>,
+    glfw_pm: &'a GlfwPlatformManager,
+}
+
+impl<'a> OpenGLRenderer<'a> {
+    fn new(gl_api: gl::Gl, glfw_pm: &'static GlfwPlatformManager) -> Box<'a + Renderer> {
+        Box::new(OpenGLRenderer {
+            gl_api: Rc::from(gl_api),
+            glfw_pm,
+        })
+    }
+}
+
+impl<'a> Renderer for OpenGLRenderer<'a> {
+    fn swap_buffer(&self) {
+        self.glfw_pm.swap_buffers()
+    }
+
+    fn clear_color(&self) {
+        unsafe {
+            self.gl_api.ClearColor(0.3, 0.3, 0.8, 1.0);
+            self.gl_api.Clear(gl::COLOR_BUFFER_BIT);
+        }
+    }
+}
+
+#[derive(Debug)]
 enum RendererType {
     None,
     OpenGL,
     Vulkan,
 }
-
 
 struct WindowConfig {
     width: u32,
@@ -39,22 +69,27 @@ struct WindowConfig {
 }
 
 trait PlatformManager {
-    fn new(config: WindowConfig) -> Box<Self> where Self: Sized;
-    fn create_renderer(&mut self, renderer_type: RendererType) -> gl::Gl;
+    fn new(config: WindowConfig) -> Box<Self> where Self:Sized;
+    fn create_renderer(&mut self, renderer_type: RendererType) -> Box<Renderer>;
     fn should_close(&self) -> bool;
     fn process_events(&mut self);
-    fn swap_buffers(&mut self);
 }
 
 struct GlfwPlatformManager {
     glfw: glfw::Glfw,
-    window: glfw::Window,
+    window: Rc<RefCell<glfw::Window>>,
     events: Receiver<(f64, glfw::WindowEvent)>,
 }
 
+impl GlfwPlatformManager {
+    fn swap_buffers(&self) {
+        self.window.borrow_mut().swap_buffers();
+    }
+}
 
-impl PlatformManager for GlfwPlatformManager {
-    fn new(config: WindowConfig) -> Box<Self> where Self: Sized {
+
+impl<'a> PlatformManager for GlfwPlatformManager {
+    fn new(config: WindowConfig) -> Box<Self> where Self:Sized {
         let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
         let (mut window, events) = glfw.create_window(config.width, config.height, "Hello this is window",
                                                       glfw::WindowMode::Windowed)
@@ -62,30 +97,35 @@ impl PlatformManager for GlfwPlatformManager {
         window.set_key_polling(true);
         window.make_current();
         window.show();
-        let manager = GlfwPlatformManager { glfw, window, events };
+        let manager = GlfwPlatformManager {
+            glfw,
+            window: Rc::from(RefCell::from(window)),
+            events,
+        };
         Box::new(manager)
     }
 
 
-    fn create_renderer(&mut self, renderer_type: RendererType) -> gl::Gl {
-        gl::Gl::load_with(|s| {
-            self.window.get_proc_address(s) as *const std::os::raw::c_void
-        })
+    fn create_renderer(&mut self, renderer_type: RendererType) -> Box<Renderer + 'static> {
+        match renderer_type {
+            RendererType::OpenGL => {
+                return OpenGLRenderer::new(gl::Gl::load_with(|s| {
+                    (*self.window).borrow_mut().get_proc_address(s) as *const std::os::raw::c_void
+                }), &*self);
+            }
+            _ => panic!("Not implemented for {:?} renderer type", renderer_type)
+        }
     }
 
     fn should_close(&self) -> bool {
-        self.window.should_close()
+        self.window.borrow().should_close()
     }
 
     fn process_events(&mut self) {
         self.glfw.poll_events();
         for (_, event) in glfw::flush_messages(&self.events) {
-            handle_window_event(&mut self.window, event);
+            handle_window_event(&mut (*self.window).borrow_mut(), event);
         }
-    }
-
-    fn swap_buffers(&mut self) {
-        self.window.swap_buffers();
     }
 }
 
