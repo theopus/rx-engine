1be9a3cfbbbabd28167955ea6fa96b694540cb7f
Oleksandr Tkachov <v-otkachov@expediagroup.com>
2019-07-26 13:50:22 +0300

    add

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,37 +3,69 @@ extern crate rx_engine;
 use rx_engine::platform::{create_pm, WindowConfig};
 use rx_engine::render::{BufferLayout, RendererType, VertexArray, VertexBuffer};
 use rx_engine::render::shared_types;
+use rx_engine::run::Layer;
+use rx_engine::run::MutLayer;
+use rx_engine::render::RendererApi;
+use rx_engine::platform::PlatformManager;
+use rx_engine::run::MutLayerBuilder;
+use rx_engine::render::RendererConstructor;
+use rx_engine::run::PushLayer;
 
+
+struct TestLayer {
+    vertex_array: Box<VertexArray>
+}
+
+impl TestLayer {
+    pub fn new(api: &RendererApi, constructor: &RendererConstructor) -> Self {
+        let mut vertex_array: Box<VertexArray> = constructor.vertex_array();
+        let mut ib = constructor.index_buffer(&[0, 1, 3, 3, 1, 2]);
+        let mut vb: Box<VertexBuffer> = constructor.vertex_buffer();
+
+        vertex_array.set_index_buffer(ib);
+
+        vb.buffer_data_f32(&[
+            -0.5_f32, 0.5_f32, 0_f32, // 0
+
+            -0.5_f32, -0.5_f32, 0_f32, // 1
+
+            0.5_f32, -0.5_f32, 0_f32, // 2
+
+            0.5_f32, 0.5_f32, 0_f32, //3
+        ]);
+        vb.set_buffer_layout(BufferLayout::with(shared_types::FLOAT_3));
+        vertex_array.add_vertex_buffer(vb);
+
+        api.set_clear_color(0.3, 0.3, 0.9, 1_f32);
+
+        TestLayer { vertex_array: vertex_array }
+    }
+}
+
+impl MutLayer for TestLayer {
+    fn on_update(&self, delta: f64, renderer_api: &mut RendererApi, platform_manager: &mut PlatformManager) {
+
+        platform_manager.process_events();
+
+
+        renderer_api.clear_color();
+        platform_manager.process_events();
+        renderer_api.draw_indexed(self.vertex_array.as_ref());
+        renderer_api.swap_buffer();
+    }
+}
+
+struct TestBuilder;
+impl MutLayerBuilder for TestBuilder {
+    fn create_layer(&mut self, api: &RendererApi, constructor: &RendererConstructor) -> Box<MutLayer> {
+        Box::new(TestLayer::new(api, constructor)) as Box<MutLayer>
+    }
+}
 
 fn main() {
-    let platform_manager = create_pm(WindowConfig { width: 600, height: 400 });
-    let (mut api, constructor) = platform_manager.create_renderer(RendererType::OpenGL);
+    let mut engine = rx_engine::run::build_engine(RendererType::OpenGL, WindowConfig { width: 600, height: 400 });
+    engine.push_layer(&mut TestBuilder{} as &mut MutLayerBuilder);
+    engine.run();
+    println!("Bye!")
 
-    let mut vertex_array: Box<VertexArray> = constructor.vertex_array();
-    let mut ib = constructor.index_buffer(&[0, 1, 3, 3, 1, 2]);
-    let mut vb: Box<VertexBuffer> = constructor.vertex_buffer();
-
-    vertex_array.set_index_buffer(ib);
-
-    vb.buffer_data_f32(&[
-        -0.5_f32, 0.5_f32, 0_f32, // 0
-
-        -0.5_f32, -0.5_f32, 0_f32, // 1
-
-        0.5_f32, -0.5_f32, 0_f32, // 2
-
-        0.5_f32, 0.5_f32, 0_f32, //3
-    ]);
-    vb.set_buffer_layout(BufferLayout::with(shared_types::FLOAT_3));
-    vertex_array.add_vertex_buffer(vb);
-
-    api.set_clear_color(0.3, 0.3, 0.9, 1_f32);
-
-
-    while !platform_manager.should_close() {
-        api.clear_color();
-        platform_manager.process_events();
-        api.draw_indexed(vertex_array.as_ref());
-        api.swap_buffer();
-    }
 }
--- core/src/run.rs
+++ core/src/run.rs
@@ -7,12 +7,28 @@ use crate::render::RendererType;
 
 pub fn build_engine(rtype: RendererType, config: WindowConfig) -> RxEngine {
     let pm: Box<PlatformManager> = create_pm(config);
-    let (renderer, constructor): (Box <RendererApi>, Box < RendererConstructor>) = pm.create_renderer(rtype);
+    let (renderer, constructor): (Box<RendererApi>, Box<RendererConstructor>) = pm.create_renderer(rtype);
     RxEngine::new(pm, renderer, constructor)
 }
 
 pub trait Layer {
-    fn on_update(&self, delta: f64) {}
+    fn on_update(&self, delta: f64);
+}
+
+
+pub trait MutLayer {
+    fn on_update(&self, delta: f64, renderer_api: &mut RendererApi, platform_manager: &mut PlatformManager);
+}
+
+
+pub trait LayerBuilder {
+    fn create_layer(&mut self, api: &RendererApi, constructor: &RendererConstructor) -> Box<Layer>;
+}
+
+
+
+pub trait MutLayerBuilder {
+    fn create_layer(&mut self, api: &RendererApi, constructor: &RendererConstructor) -> Box<MutLayer>;
 }
 
 pub struct RxEngine {
@@ -20,7 +36,27 @@ pub struct RxEngine {
     renderer: Box<dyn RendererApi>,
     renderer_constructor: Box<dyn RendererConstructor>,
     layers: Vec<Box<dyn Layer>>,
+    mut_layers: Vec<Box<dyn MutLayer>>,
 }
+
+pub trait PushLayer<T> {
+    fn push_layer(&mut self, t: T);
+}
+
+
+impl PushLayer<&mut LayerBuilder> for RxEngine {
+    fn push_layer(&mut self, layer_builder: &mut LayerBuilder) {
+        let l = layer_builder.create_layer(self.renderer.as_ref(), self.renderer_constructor.as_ref());
+        self.layers.push(l);
+    }
+}
+impl PushLayer<&mut MutLayerBuilder> for RxEngine {
+    fn push_layer(&mut self, layer_builder: &mut MutLayerBuilder) {
+        let l = layer_builder.create_layer(self.renderer.as_ref(), self.renderer_constructor.as_ref());
+        self.mut_layers.push(l);
+    }
+}
+
 
 impl RxEngine {
     pub fn new(
@@ -28,12 +64,29 @@ impl RxEngine {
         renderer: Box<RendererApi>,
         renderer_constructor: Box<RendererConstructor>,
     ) -> RxEngine {
-        RxEngine { platform, renderer, renderer_constructor, layers: Vec::new() }
+        RxEngine { platform, renderer, renderer_constructor, layers: Vec::new(), mut_layers: Vec::new() }
     }
-    pub fn push_layer() {}
+    pub fn run(&mut self) {
+        while self.should_run() {
+            for l in self.layers.iter() {
+                l.on_update(0_f64);
+            }
+            for l in self.mut_layers.iter_mut() {
+                l.on_update(0_f64,
+                            self.renderer.as_mut(),
+                            self.platform.as_mut());
+            }
+        }
+    }
+
+    fn should_run(&self) -> bool {
+        !self.platform.should_close()
+    }
+}
+
+impl Drop for RxEngine {
+    fn drop(&mut self) {
+    }
 }
 
 
-pub trait LayerBuilder {
-    fn create_layer<T>(api: &RendererApi, constructor: &RendererConstructor) -> Box<T> where T: Layer;
-}
